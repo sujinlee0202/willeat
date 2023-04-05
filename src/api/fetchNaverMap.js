a31fa3dbb974cab3ffbdd7e3765a39a025cacdb0
titch <sujinlee0202@gmail.com>
2023-04-05 16:32:38 +0900

    EditModal 지도 기능 추가 - 네이버 지도 api, Tmap 좌표 변환 api, proxy 설정

--- src/api/fetchNaverMap.js
+++ src/api/fetchNaverMap.js
@@ -1,7 +1,32 @@
-export const initMap = () => {
+import { convertGeo } from "./tmap";
+
+export const initMap = (x, y) => {
   let mapDiv = document.getElementById('map')
   const map = new naver.maps.Map(mapDiv, {
     center: new naver.maps.LatLng(37.511337, 127.012084),
     zoom: 13,
   });
+}
+
+export const searchMap = async (x, y) => {
+  let convertX = 0
+  let convertY = 0
+
+  await convertGeo(x, y)
+  .then(res => Object.values(res.data)[0])
+  .then(coord => {
+    convertX = coord.lat
+    convertY = coord.lon
+    return convertX, convertY
+  })
+
+  let mapDiv = document.getElementById('searchMap')
+  const map = new naver.maps.Map(mapDiv, {
+    center: new naver.maps.LatLng(convertX, convertY),
+    zoom: 19,
+  });
+  const marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(convertX, convertY),
+    map: map
+  })
 }
--- src/components/EditModal/index.jsx
+++ src/components/EditModal/index.jsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 import styles from './styles.module.css'
 import { uploadImage } from '../../api/cloudinary'
 import { addNewPlace } from '../../api/firebase'
-// import addNewPlace from '../../api/firebase'
+import { searchNaver } from '../../api/fetchNaverPlace'
+import { searchMap } from '../../api/fetchNaverMap'
 
 const EditModal = ({onClick, setOpenEditModal}) => {
   const [file, setFile] = useState()
   const [isUploading, setIsUploading] = useState(false)
   const [place, setPlace] = useState({})
   const [imageUrl, setImageUrl] = useState()
+  const [openMap, setOpenMap] = useState(false)
 
   const onChangeInput = (e) => {
     const {id, value, files} = e.target
@@ -19,16 +21,29 @@ const EditModal = ({onClick, setOpenEditModal}) => {
       reader.onload = () => {
         setImageUrl(reader.result)
       }
-      reader.readAsDataURL(file)
+      reader.readAsDataURL(files && files[0])
       return
     }
 
     setPlace(place => ({...place, [id]: value}))
   }
 
+  const onClickSearch = (e) => {
+    e.preventDefault()
+    setOpenMap(true)
+
+    searchNaver(place.name)
+    .then(res => res.data.items[0]) // mapx, mapy
+    .then(item => {
+      searchMap(item.mapx, item.mapy)
+      setPlace(place => ({...place, address: item.roadAddress}))
+    })
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
     setIsUploading(true)
+
     uploadImage(file)
     .then(url => addNewPlace(place, url))
     .then(() => setTimeout(() => alert('성공적으로 지도에 추가됬습니다'), 1000))
@@ -40,33 +55,88 @@ const EditModal = ({onClick, setOpenEditModal}) => {
   }
 
   return (
-    <form className={styles.modalContainer} onClick={onClick} onSubmit={handleSubmit}>
-      {imageUrl 
-        ? <img src={imageUrl} htmlFor='file' alt={place.name} className={styles.image}></img>
-        : <label htmlFor='file' className={styles.fileButton}>파일 업로드</label>}
-      <input type='file' id='file' className={styles.inputFile} onChange={onChangeInput} required></input>
-      <div className={styles.fileButton}>위치</div>
-      <div>
-        <label>이름 :</label>
-        <input type='text' id='name' placeholder='음식점 이름' value={place.name ?? ''} onChange={onChangeInput} required></input>
-        <button>검색</button>
-      </div>
-      <div>
-        <label>카테고리 :</label>
-        <input type='text' id='category' placeholder='카테고리' value={place.category ?? ''} onChange={onChangeInput} required></input>
-      </div>
-      <div>
-        <label>소개글 :</label>
-        <input type='text' id='description' placeholder='음식점에 대한 소개글 작성' value={place.description ?? ''} onChange={onChangeInput} required></input>
-      </div>
-      <div>
-        <label>태그 :</label>
-        <input type='text' id='tag' placeholder='키워드 및 태그 (, 로 구분)' value={place.tag ?? ''} onChange={onChangeInput} required></input>
-      </div>
-      <button>
-        {isUploading ? '업로드 중...' : '장소 등록하기'}
-      </button>
-    </form>
+    <div className={styles.modalContainer} onClick={onClick}>
+      <form className={styles.searchForm}>
+        <label className={styles.labelText}>이름 :</label>
+        <input 
+          type='text' 
+          id='name' 
+          placeholder='음식점 이름' 
+          value={place.name ?? ''}
+          className={styles.input}
+          onChange={onChangeInput} 
+          required
+        />
+        <button onClick={onClickSearch} className={styles.btnSearch}>검색</button>
+      </form>
+      <form 
+        onSubmit={handleSubmit} 
+        className={styles.formContainer}
+      >
+        <div className={styles.fileButton}>
+          {openMap 
+            ? <div id='searchMap' className={styles.map}></div> 
+            :'위치'}
+        </div>
+        {imageUrl 
+          ? <img src={imageUrl} htmlFor='file' alt={place.name} className={styles.image} />
+          : <label htmlFor='file' className={styles.fileButton}>파일 업로드</label>}
+        <input 
+          type='file' 
+          id='file' 
+          className={styles.inputFile} 
+          onChange={onChangeInput}
+        />
+        <input 
+          type='text' 
+          id='name' 
+          placeholder='음식점 이름' 
+          value={place.name ?? ''} 
+          onChange={onChangeInput} 
+          className={styles.inputName} 
+          required
+        />
+        <div className={styles.inputContainer}>
+          <label>카테고리 :</label>
+          <input 
+            type='text' 
+            id='category' 
+            placeholder='카테고리' 
+            value={place.category ?? ''}
+            className={styles.input}
+            onChange={onChangeInput} 
+            required
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>소개글 :</label>
+          <input 
+            type='text' 
+            id='description' 
+            placeholder='음식점에 대한 소개글 작성' 
+            value={place.description ?? ''} 
+            className={styles.input}
+            onChange={onChangeInput} 
+            required
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>태그 :</label>
+          <input 
+            type='text'
+           id='tag' 
+           placeholder='키워드 및 태그 (, 로 구분)' 
+           value={place.tag ?? ''} 
+           className={styles.input}
+           onChange={onChangeInput} 
+           required
+          />
+        </div>
+        <button className={styles.btnUpload}>
+          {isUploading ? '업로드 중...' : '장소 등록하기'}
+        </button>
+      </form>
+    </div>
   )
 }
 
